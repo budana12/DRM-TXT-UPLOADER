57c565625635c356b8892fa73a74f5b028edb5f2
budana12 <sirowabudana@gmail.com>
2025-05-06 23:25:04 +0530

    config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = os.environ.get("API_ID", "10768857") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "81b5d675d410f16ceb39df10db9cdb54") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8122988957:AAG91AJVb5VefW1n4I-2OChf00TIjgkzsso") #Replace With your bot token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7464498219:AAFacXLo-pvsNBWog8jgXtd3YU0pLukVvJ0") #Replace With your bot token
